99ba61721a1ab047f660f6a963aac3f4c45ac608
Mel Gray <melgray@gmail.com>
2018-01-03 18:31:01 -0600

    Adding the bones for a basic h264 decoder

--- Sources/memento/memento.swift
+++ Sources/memento/memento.swift
@@ -1,2 +1,77 @@
 import Foundation
 import Clibavcodec
+
+
+final public class H264Decoder {
+
+    fileprivate let IN_BUF_SIZE = 4096
+    
+    fileprivate var codec: AVCodec
+    fileprivate var context: AVCodecContext
+    fileprivate var parser: AVCodecParserContext
+    fileprivate var packet: AVPacket
+    fileprivate var picture: AVFrame
+    
+    fileprivate var decodeCallback: (Data) -> Void
+    
+    public init(decodeCallback: @escaping (Data) -> Void) {
+        avcodec_register_all()
+        self.decodeCallback = decodeCallback
+        self.codec          = avcodec_find_decoder(AV_CODEC_ID_H264).pointee
+        self.parser         = av_parser_init(Int32(self.codec.id.rawValue)).pointee
+        self.context        = avcodec_alloc_context3(&self.codec).pointee
+        self.packet         = av_packet_alloc().pointee
+        self.picture        = av_frame_alloc().pointee
+        
+        avcodec_open2(&self.context, &self.codec, nil)
+    }
+
+    func decode(_ payload: [UInt8]) {
+        var safePayload = payload
+        
+        var cnt: Int32 = 0
+        while cnt < payload.count {
+            
+            var ptr: UnsafeMutablePointer<UInt8>? = UnsafeMutablePointer.allocate(capacity: 4096)
+            var size: Int32 = 0
+
+            var ret = av_parser_parse2(&parser,
+                                       &context,
+                                       &ptr,
+                                       &size,
+                                       &safePayload,
+                                       Int32(payload.count),
+                                       0x80,
+                                       0x80,
+                                       0)
+
+            if size > 0 {
+                packet.data = ptr
+                packet.size = size
+
+                ret = avcodec_send_packet(&context, &self.packet)
+                if ret < 0 {
+                    print("avcodec_send_packet problem")
+                    continue
+                }
+                
+                ret = avcodec_receive_frame(&self.context, &self.picture)
+                if ret < 0 {
+                    print(self.picture)
+                    print("error decoding, avcodec_receive_frame", ret)
+                    continue
+                }
+                
+                print(picture)
+
+                cnt += 1
+            } else {
+                cnt += ret-1
+            }
+
+            
+        }
+        
+    }
+    
+}
--- Tests/mementoTests/mementoTests.swift
+++ Tests/mementoTests/mementoTests.swift
@@ -6,12 +6,16 @@ class mementoTests: XCTestCase {
     func test_that_we_can_decode_an_h264_encoded_iframe() {
         
         let fixturesURL = URL(fileURLWithPath: fixturesPath)
-        let iFrameURL   = fixturesURL.appendingPathComponent("0.h264")
+        let iFrameURL   = fixturesURL.appendingPathComponent("test.264")
         let iFrameData = try? Data(contentsOf: iFrameURL)
         XCTAssertNotNil(iFrameData)
+        
+        let iFramePayload =  Array<UInt8>(iFrameData!)
 
-        let decoder = H264Decoder()
-        decoder.decode(iFrameData!)
+        let decoder = H264Decoder() { img in
+            
+        }
+        decoder.decode(iFramePayload)
         
     }
 
