647ef9b37700e016deb6d6692e1a2436d8cd7ac7
krad <iam@krad.io>
2018-01-04 18:09:31 -0600

    Updating to use the github url in the package

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["memento"]),
     ],
     dependencies: [
-        .package(url: "./Clibavcodec", from: "0.0.1"),
+        .package(url: "https://www.github.com/krad/Clibavcodec.git", from: "0.0.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
