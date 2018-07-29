3c87d51ab18e16ecc16bc45b0be38253d017013a
krad <iam@krad.io>
2018-07-29 14:46:54 -0500

    Small API refactor to make it easier to communicate with quinn and leonard

--- Sources/memento/memento.swift
+++ Sources/memento/memento.swift
@@ -1,5 +1,7 @@
 import Foundation
 
+let delimiter: [UInt8] = [0x0, 0x0, 0x0, 0x1]
+
 public protocol MementoProtocol {
     func wroteJPEG(to url: URL)
     func failedToWriteJPEG(error: Error)
@@ -44,15 +46,31 @@ public struct Base64EncodedThumbnailRequest: Codable {
     var sps: String?
     var pps: String?
     var payload: String
+    
+    var config: ThumbnailConfig?
+    
+    var spsBytes: [UInt8]? { return decodeBase64(self.sps) }
+    var ppsBytes: [UInt8]? { return decodeBase64(self.pps) }
+    var payloadBytes: [UInt8] { return decodeBase64(self.payload)! }
+}
+
+func decodeBase64(_ key: String?) -> [UInt8]? {
+    guard let str = key else { return nil }
+    if let data = Data(base64Encoded: str, options: .ignoreUnknownCharacters) {
+        return [UInt8](data)
+    }
+    return nil
 }
 
 public enum MementoError: Error {
+    case thumbnailEncodeFailed
+    case h264DecodeFailed
+    case needDecoderConfig
     case failedToEncode
 }
 
 final public class Memento {
     
-    private let delimiter: [UInt8] = [0x0, 0x0, 0x0, 0x1]
     private var sps: [UInt8]?
     private var pps: [UInt8]?
     
@@ -63,6 +81,23 @@ final public class Memento {
     final public func set(sps: [UInt8], pps: [UInt8]) {
         self.sps = sps
         self.pps = pps
+    }
+    
+    final public func decode(_ req: Base64EncodedThumbnailRequest) throws -> Data? {
+        if let sps = req.spsBytes, let pps = req.ppsBytes {
+            let payload = buildPayload(sps: sps, pps: pps, frame: req.payloadBytes)
+            if let decContext = self.h264Decoder.decode(payload) {
+                let jpegEncoder = JPEGEncoder(decodeContext: decContext)
+                if let pic = jpegEncoder.encode(with: decContext.frame) {
+                    return pic
+                } else {
+                    throw MementoError.thumbnailEncodeFailed
+                }
+            } else {
+                throw MementoError.h264DecodeFailed
+            }
+        }
+        throw MementoError.needDecoderConfig
     }
     
     final public func decode(keyframe: [UInt8]) throws -> Data? {
@@ -85,4 +120,12 @@ final public class Memento {
         throw MementoError.failedToEncode
     }
     
+}
+
+/// FIXME: Make this more robust
+func buildPayload(sps: [UInt8], pps: [UInt8], frame: [UInt8]) -> [UInt8] {
+    var payload: [UInt8] = delimiter + sps
+    payload += delimiter + pps
+    payload += delimiter + frame
+    return payload
 }
